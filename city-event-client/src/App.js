982240033cf114f273df794d3098ecf2e9ed5f48
Korish <boybkorish@gmail.com>
2020-02-12 10:28:46 +0200

    some practice with react

--- city-event-client/src/App.js
+++ city-event-client/src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    state = {
+        persons: [
+            {name: 'Alex', age: 18},
+            {name: 'Den', age: 23},
+            {name: 'Ben', age: 14}
+        ]
+    }
+
+    switchNameHandler = () =>{
+        // console.log('Was clicked!');
+        // WON`T WORK !!!  this.state.persons[0].name = 'Maximilian;'
+
+        this.setState({
+            persons: [
+                {name: 'Korish', age: 38},
+                {name: 'Den', age: 23},
+                {name: 'Anna', age: 22}
+            ]
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Hi everyone Guys</h1>
+                    <button onClick={this.switchNameHandler} className='btn'>Switch Name</button>
+                    <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                    <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                    <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                </header>
+            </div>
+        );
+        // return React.createElement("div", {className : "App"}, "h2", "sdjknvjksdnvsdnv");
+    }
+
 }
 
 export default App;
---
+++ city-event-client/src/Person/Person.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const person = (props) => {
+
+
+
+    return (
+        <div>
+            <p>I`m {props.name} and  {props.age} y.o </p>
+            <p>{props.children}</p>
+        </div>
+    );
+
+}
+
+export default person;
